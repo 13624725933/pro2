6f38446089b7306d4bd1839cd7bfc45d402ec40d
chenbiao <chenbiao@itcast.com>
2018-11-25 18:57:57 +0800

    提交

---
+++ src/com/itheima/GitTest.java
@@ -0,0 +1,12 @@
+package com.itheima;
+
+public class GitTest {
+    public static void main(String[] args) {
+        System.out.println("hello GIT ");
+        System.out.println("hello GIT ");
+        System.out.println("hello GIT ");
+        System.out.println("hello GIT ");
+        System.out.println("hello GIT ");
+        System.out.println("hello GIT ");
+    }
+}
